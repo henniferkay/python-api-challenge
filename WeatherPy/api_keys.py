8ac353013fdadc5e88735eed15f1acce4ea197f5
henniferkay <kimjen104@MacBook-Pro-6.fios-router.home>
2023-12-13 18:53:26 -0500

    Updated code

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "b1fda3dd8c7e2755e5ea00571d8c34d8"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "33a96b0deb7648008abc1d289d67bdd8"
